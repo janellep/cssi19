c0a3652d178feaffff24ff27ad77b99717a0ccbf
janellep <janellepiedrahita@mec.science>
2019-07-29 16:39:19 -0400

    alarmLab

---
+++ Day5/alarmLab/5.js
@@ -0,0 +1,42 @@
+const basicAlarm = (alarmMessage) => {
+  return alarmMessage;
+}; 
+
+console.log(basicAlarm("My alarm!"));
+
+
+
+function myAlarm(wakeup){
+console.log("Hey, s. wakeup. It's" +wakeup);
+}
+
+myAlarm("4:30am");
+
+function momalarm(wakeup){
+    console.log("MOM! GET UP IT'S" +wakeup);
+}
+momalarm('9:00am');
+
+function familyalarm(wakeup,person){
+  console.log("hey ,person , IT'S", wakeup);
+}
+familyalarm("nelle",'11:00am');
+
+function importantalarm(wakeup){
+    return wakeup.toUpperCase();
+}
+console.log(importantalaram("wakeup!"))
+ 
+
+
+function snooze(wakeup){
+  console.log("bruh stay sleep" +wakeup);
+}
+
+snooze('3:30');
+
+
+
+
+
+
