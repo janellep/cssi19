90b7b200061ebadf562d7bde8c4a99628370b832
janellep <janellepiedrahita@mec.science>
2019-07-30 16:57:59 -0400

    Day 6 work

---
+++ Day6/script.js
@@ -0,0 +1,31 @@
+// const CountToN=(x)=>{
+// }
+
+function CountToN(x){
+    let absx = Math.abs(x);
+    let index=1;
+    while(index<= absx){
+        console.log(index);
+        index=index+1;
+    }
+}
+
+CountToN(4);
+
+ const numberPrompt = () =>{ 
+
+     while(true)
+
+     let response = Number(prompt('enter a number'))
+
+     if(!NaN(response))
+
+     return response;
+            ]
+        }
+  const getNumber = () =>{
+
+  let num = numberPrompt
+  console.log(num + 10 );
+
+  }
---
+++ Day6/script2.js
@@ -0,0 +1,12 @@
+let array= []
+
+function multiples(mult,m,n){
+    
+    for(i=1;i<n;i++){
+        n.push(m*i);
+    }
+    for(i=0;i<n.length;i++){
+        console.log(n[i]);
+    }
+}
+ mult(4,2,array);
